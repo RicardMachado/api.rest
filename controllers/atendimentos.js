6e30048275f32b187de0056825c5c7c3209c259d
Ricardo M <ricardo.machado10.rm@gmail.com>
2021-01-16 11:14:44 -0300

    Criação de rotas POST e GET

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,9 @@
 module.exports = app => {
-    app.get('/atendimento', (req,res) => res.send('Voçe está na rota de atendimento!'))
+    app.get('/atendimento', (req, res) => res.send('Voçe está na rota de atendimento que está realizando um GET'))
+
+    app.post('/atendimento', (req, res) => {
+     
+        console.log(req.body)
+        res.send('Voçe está na rota de atendimento que está realizando um POST')
+    })
 } 
