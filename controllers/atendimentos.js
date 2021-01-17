0239d0206b754eb885e1b3c8f53a7f973ea7f870
Ricardo M <ricardo.machado10.rm@gmail.com>
2021-01-16 21:45:52 -0300

    Criação de tabelas com script node

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,13 @@
+const Atendimento = require ('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimento', (req, res) => res.send('Voçe está na rota de atendimento que está realizando um GET'))
 
     app.post('/atendimento', (req, res) => {
      
-        console.log(req.body)
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
         res.send('Voçe está na rota de atendimento que está realizando um POST')
     })
 } 
